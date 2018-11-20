15ba14b910c931185eefde9dbe526ee9cc887d28
Gabriel Zalisz <gabriel.zalisz@trz.cz>
2018-11-20 15:57:44 +0100

    song class

---
+++ HelloSwipeViewWithTabs/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloSwipeViewWithTabs
+{
+    public class Song
+    {
+        public int Numer { get; set; }
+
+        public string Tytul { get; set; }
+
+        public string Tonacja { get; set; }
+
+        public string Slowa { get; set; }
+    }
+}
