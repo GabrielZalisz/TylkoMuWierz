2a3b2a8ec75b05e0d7bc5af4e72a1816884bca95
Gabriel Zalisz <gabriel.zalisz@trz.cz>
2018-12-13 07:50:31 +0100

    WriteExternalStorage permission removed

--- TMWforAndroid/Properties/AssemblyInfo.cs
+++ TMWforAndroid/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloSwipeViewWithTabs")]
+[assembly: AssemblyTitle("TylkoMuWierz")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloSwipeViewWithTabs")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("TylkoMuWierz")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -30,5 +30,5 @@ using Android.App;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+//[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+//[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
